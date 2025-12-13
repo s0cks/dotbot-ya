e19ddf1093020e005e57355b68a8cae8c74f542c
tazz <tazz@tazz.cloud>
2025-12-13 16:14:22 -0500

    feat: :sparkles: fix to actually install ya packages properly

--- ya.py
+++ ya.py
@@ -1,36 +1,50 @@
 import subprocess, dotbot, os
 from pprint import PrettyPrinter
 
-def run_command(self, command):
-  try:
-    if isinstance(command, list):
-      command = ' '.join(command)
-    elif not isinstance(command, str):
-      command = str(command)
-    subprocess.run([command], shell=True, check=True)
-    return True
-  except Exception as ex:
-    self._log.error(f"failed to run command: ")
-    return False
+class bcolors:
+  HEADER = '\033[95m'
+  OKBLUE = '\033[94m'
+  OKCYAN = '\033[96m'
+  OKGREEN = '\033[92m'
+  WARNING = '\033[93m'
+  FAIL = '\033[91m'
+  ENDC = '\033[0m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
+  UWHITE = '\033[4;37m'
+
+def _run_command(command):
+  if isinstance(command, list):
+    command = ' '.join(command)
+  elif not isinstance(command, str):
+    command = str(command)
+  subprocess.run([command], shell=True, check=True)
 
 class YaPlugin:
   def __init__(self, name):
-    self._name = name
+    self.name = name
+    self.command = [
+      'ya',
+      'pkg',
+      'add',
+      name,
+    ]
 
-  def get_command(self):
-    command = []
-    command.append('ya')
-    command.append('pkg')
-    command.append('add')
-    command.append(self._name)
-    return command
+  def is_installed(self):
+    try:
+      _run_command(f"$(ya pkg list | grep {self.name} &>/dev/null)")
+      return True
+    except Exception as ex:
+      return False
 
   def install(self):
-    #TODO(@s0cks): should check if plugin needs to be installed
-    return run_command(self.get_command())
+    if self.is_installed():
+      print(f"\t- {bcolors.OKCYAN} {bcolors.BOLD}{bcolors.UWHITE}{self.name}{bcolors.ENDC} already installed, skipping")
+      return
+    _run_command(self.command)
 
   def __str__(self):
-    return self._name
+    return self.name
 
 
 __version__ = "0.0.1"
@@ -50,12 +64,20 @@ class DotbotYa(dotbot.Plugin):
       self._log.error(ex.args[0])
       return False
 
+    print(f"installing {len(data)} yazi plugins....")
     num_installed = 0
-    for plugin in data:
+    for name in data:
       try:
-        self._install(plugin)
+        pkg = YaPlugin(name)
+        pkg.install()
         num_installed = num_installed + 1
       except Exception as ex:
         self._log.error(f"failed to install the {plugin} plugin")
-    return num_installed == length(data)
+
+    if num_installed == len(data):
+      status = f"{bcolors.OKGREEN} {bcolors.ENDC}"
+    else:
+      status = f"{bcolors.RED} {bcolors.ENDC}"
+    print(f"\t{status} installed {num_installed}/{len(data)} yazi plugins")
+    return num_installed == len(data)
 
