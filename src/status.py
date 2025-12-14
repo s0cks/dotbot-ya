bfd93f98a5029d2d757a3c59847c175e78a5c796
tazz <tazz@tazz.cloud>
2025-12-14 04:36:06 -0500

    refactor: :recycle: add some more code cleanup

--- src/status.py
+++ src/status.py
@@ -4,23 +4,24 @@ SUCCESS = 0
 SKIPPED = 1
 FAILED = 2
 
+
 class YaPluginStatus:
-  def __init__(self, pkg, status):
-    self.pkg = pkg
-    self.status = status
+    def __init__(self, pkg, status):
+        self.pkg = pkg
+        self.status = status
 
-  def is_success(self):
-    return self.status == SUCCESS
-  
-  def is_skipped(self):
-    return self.status == SKIPPED
+    def is_success(self):
+        return self.status == SUCCESS
 
-  def is_failed(self):
-    return self.status == FAILED
+    def is_skipped(self):
+        return self.status == SKIPPED
 
-  def __str__(self):
-    if self.is_success():
-      return f"{bcolors.OKGREEN} {bcolors.BOLD}{bcolors.WHITE}{self.pkg}{bcolors.ENDC} installed."
-    elif self.is_skipped():
-      return f"{bcolors.WARNING}! {bcolors.BOLD}{bcolors.WHITE}{self.pkg}{bcolors.ENDC} is already installed, skipping"
-    return f"{bcolors.RED} {bcolors.BOLD}{bcolors.WHITE}{self.pkg}{bcolors.ENDC} failed to insall"
+    def is_failed(self):
+        return self.status == FAILED
+
+    def __str__(self):
+        if self.is_success():
+            return f"{bcolors.OKGREEN} {bcolors.BOLD}{bcolors.WHITE}{self.pkg}{bcolors.ENDC} installed."
+        elif self.is_skipped():
+            return f"{bcolors.WARNING}! {bcolors.BOLD}{bcolors.WHITE}{self.pkg}{bcolors.ENDC} is already installed, skipping"
+        return f"{bcolors.RED} {bcolors.BOLD}{bcolors.WHITE}{self.pkg}{bcolors.ENDC} failed to insall"
--- ya.py
+++ ya.py
@@ -1,44 +1,45 @@
-import subprocess, dotbot, os
-from pprint import PrettyPrinter
-
+import subprocess, dotbot
 from src import YaPlugin, bcolors
 
 __version__ = "0.0.1"
+
+
 class DotbotYa(dotbot.Plugin):
-  def can_handle(self, directive):
-    valid = directive == "ya"
-    if not valid:
-      self._log.debug(f"The ya plugin doesn't support the `{directive}` directive")
-    return valid
+    def can_handle(self, directive):
+        valid = directive == "ya"
+        if not valid:
+            self._log.debug(
+                f"The ya plugin doesn't support the `{directive}` directive"
+            )
+        return valid
 
-  def handle(self, directive, data):
-    if not self.can_handle(directive):
-      return False
-    try:
-      pass
-    except ValueError as ex:
-      self._log.error(ex.args[0])
-      return False
+    def handle(self, directive, data):
+        if not self.can_handle(directive):
+            return False
+        try:
+            pass
+        except ValueError as ex:
+            self._log.error(ex.args[0])
+            return False
 
-    print(f"installing {len(data)} yazi plugins....")
-    num_installed = 0
-    num_skipped = 0
-    num_failed = 0
-    for name in data:
-      pkg = YaPlugin(name)
-      status = pkg.install()
-      print(status)
-      if status.is_success():
-        num_installed = num_installed + 1
-      elif status.is_failed():
-        num_failed = num_failed + 1
-      elif status.is_skipped():
-        num_skipped = num_skipped + 1
+        print(f"installing {len(data)} yazi plugins....")
+        num_installed = 0
+        num_skipped = 0
+        num_failed = 0
+        for name in data:
+            pkg = YaPlugin(name)
+            status = pkg.install()
+            print(status)
+            if status.is_success():
+                num_installed = num_installed + 1
+            elif status.is_failed():
+                num_failed = num_failed + 1
+            elif status.is_skipped():
+                num_skipped = num_skipped + 1
 
-    if (num_installed + num_skipped) == len(data):
-      status = f"{bcolors.OKGREEN} {bcolors.ENDC}"
-    else:
-      status = f"{bcolors.RED} {bcolors.ENDC}"
-    print(f"{status} installed {num_installed}/{len(data)} yazi plugins")
-    return (num_installed + num_skipped) == len(data)
-
+        if (num_installed + num_skipped) == len(data):
+            status = f"{bcolors.OKGREEN} {bcolors.ENDC}"
+        else:
+            status = f"{bcolors.RED} {bcolors.ENDC}"
+        print(f"{status} installed {num_installed}/{len(data)} yazi plugins")
+        return (num_installed + num_skipped) == len(data)
